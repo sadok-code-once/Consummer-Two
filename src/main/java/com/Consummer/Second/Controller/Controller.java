7e769beef946d1c27a94e2da51f46498f88d6003
sadok-code-once <sadok.sbaa@codeonce.fr>
2023-07-04 09:41:30 +0100

    commit

---
+++ src/main/java/com/Consummer/Second/Controller/Controller.java
@@ -0,0 +1,26 @@
+package com.Consummer.Second.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+List<String> messages = new ArrayList<>();
+	
+	@GetMapping("/consumeStringMessage")
+	public List<String> consumeMsg() {
+		return messages;
+	}
+	
+	@KafkaListener(groupId = "TwoListeners", topicPartitions = @TopicPartition(topic = "topic", partitions = {"1","2"}))
+	public List<String> getMsgFromTopic(String data) {
+		messages.add(data);
+		return messages;
+	}
+	
+}
